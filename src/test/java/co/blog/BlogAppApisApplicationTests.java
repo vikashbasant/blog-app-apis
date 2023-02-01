394ac1adc76615865f87d04d0e5164538035d654
Vikash Basant <basantvikash360@gmail.com>
2023-02-02 01:32:03 +0530

    Project Setup

---
+++ src/test/java/co/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package co.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
