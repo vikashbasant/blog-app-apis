5790879825a5e30b38853e0fe80e60f63bd02fbf
Vikash Basant <basantvikash360@gmail.com>
2023-02-19 13:08:11 +0530

    some validation modification in categoryDTO

--- src/main/java/co/blog/payloads/cDTO/CategoryDTO.java
+++ src/main/java/co/blog/payloads/cDTO/CategoryDTO.java
@@ -13,12 +13,12 @@ public class CategoryDTO {
 
     @NotNull(message = "categoryTitle can't be Null")
     @NotEmpty(message = "categoryTitle can't be Empty")
-    @Size(max = 300, message = "categoryTitle maximum 500 character are allowed")
+    @Size(min = 3, max = 300, message = "categoryTitle minimum 3 & maximum 300 character are allowed")
     private String categoryTitle;
 
 
     @NotNull(message = "categoryDescription can't be Null")
     @NotEmpty(message = "categoryDescription can't be Empty")
-    @Size(max = 800, message = "categoryDescription maximum 800 character are allowed")
+    @Size(min = 10, max = 800, message = "categoryDescription minimum 10 & maximum 800 character are allowed")
     private String categoryDescription;
 }
