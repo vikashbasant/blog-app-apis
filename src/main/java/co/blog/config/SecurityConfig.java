8ab0a24e3bfcdc9e94dda6de7aff07121a586e2f
Vikash Basant <basantvikash360@gmail.com>
2023-03-14 22:26:34 +0530

    inside SecurityConfig.java class, Spring security without the WebSecurityConfigurerAdapter, New way of configuration Spring Security

--- src/main/java/co/blog/config/SecurityConfig.java
+++ src/main/java/co/blog/config/SecurityConfig.java
@@ -10,7 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +20,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -27,7 +31,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 
     @Autowired
@@ -39,6 +43,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("===: SecurityConfig:: Inside securityFilterChain Method :===");
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(SecurityConfigConstants.PUBLIC_URL).permitAll()
+                .antMatchers(HttpMethod.GET).permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.authenticationProvider(daoAuthenticationProvider());
+
+        DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
+        return defaultSecurityFilterChain;
+    }
+
+    /*
     @Override
     protected void configure (HttpSecurity http) throws Exception {
         log.info("===: SecurityConfig:: Inside configure Method :===");
@@ -59,12 +91,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
+    */
 
-    @Override
+    /*@Override
     protected void configure (AuthenticationManagerBuilder auth) throws Exception {
         log.info("===: SecurityConfig:: Inside configure Method :===");
         auth.userDetailsService(this.customUserDetailService).passwordEncoder(getPasswordEncoder());
-    }
+    }*/
+
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -72,10 +107,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    /*@Bean
     @Override
     public AuthenticationManager authenticationManagerBean () throws Exception {
         log.info("===: SecurityConfig:: Inside authenticationManagerBean Method :===");
         return super.authenticationManagerBean();
+    }*/
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        log.info("===: SecurityConfig:: Inside daoAuthenticationProvider Method:===");
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(this.customUserDetailService);
+        provider.setPasswordEncoder(getPasswordEncoder());
+        return provider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManagerBean (AuthenticationConfiguration configuration) throws Exception {
+        log.info("===: SecurityConfig:: Inside authenticationManagerBean Method :===");
+        return configuration.getAuthenticationManager();
     }
 }
