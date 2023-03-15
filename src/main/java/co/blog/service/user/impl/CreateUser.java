62642ef03363b223a3073910e89e55785d80df24
Vikash Basant <basantvikash360@gmail.com>
2023-03-15 10:57:48 +0530

    RegisterUser and CreateUser Api are same, some logic change in CreateUser

--- src/main/java/co/blog/service/user/impl/CreateUser.java
+++ src/main/java/co/blog/service/user/impl/CreateUser.java
@@ -1,17 +1,20 @@
 package co.blog.service.user.impl;
 
 import co.blog.constants.BlogAppConstants;
+import co.blog.entity.Role;
 import co.blog.entity.User;
 import co.blog.exception.GeneralException;
 import co.blog.payloads.Response;
 import co.blog.payloads.userdto.UserDTO;
 import co.blog.payloads.userdto.UserResponseDTO;
+import co.blog.repository.RoleRepo;
 import co.blog.repository.UserRepo;
 import co.blog.util.BlogService;
 import co.blog.util.BlogServiceType;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,6 +25,9 @@ public class CreateUser implements BlogService {
     private UserRepo userRepo;
 
     @Autowired
+    private RoleRepo rRepo;
+
+    @Autowired
     private Response response;
 
     @Autowired
@@ -29,6 +35,9 @@ public class CreateUser implements BlogService {
 
     @Autowired
     private UserResponseDTO uResponseDTO;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public BlogServiceType getServiceType () {
@@ -44,6 +53,19 @@ public class CreateUser implements BlogService {
 
         /*----Convert userDto into user:----*/
         User user = this.userDtoToUser(uDTO);
+
+        /*---- Now We have Encoded The Password ----*/
+        user.setUserPassword(this.passwordEncoder.encode(user.getUserPassword()));
+
+
+        // For Roles:
+        int userRoleId = uDTO.getUserRoleId();
+        /*----If Any One Register through Register API, Then we can assign the NORMAL User----*/
+        Role role = this.rRepo.findById(userRoleId).orElseThrow(() -> new GeneralException("Role Not " +
+                "Found With RoleId = " + userRoleId));
+
+        /*----Now Simply Add the Role into User----*/
+        user.getRoles().add(role);;
 
         /*----Save User into DB:----*/
         User sUser = userRepo.save(user);
